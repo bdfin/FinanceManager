f2b6e2dfd61bf2c199b347415c4704594078bba5
Beau <beau.d.findlay@gmail.com>
2021-02-08 20:22:33 +0000

    Add user helpers.

---
+++ FinanceManager/FinanceManager/Helpers/UserHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Helpers
+{
+    public static class UserHelpers
+    {
+        public static string GetUserName(AuthenticationState authState)
+        {
+            return authState.User.Claims.FirstOrDefault(t => t.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname").Value;
+        }
+
+        public static string GetUserId(AuthenticationState authState)
+        {
+            return authState.User.Claims.FirstOrDefault(c => c.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+        }
+    }
+}
